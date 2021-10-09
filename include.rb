261bd633cd6d1d32fd4881ce1b180cbd4d6e1e25
Rey <sm8967724231@gmail.com>
2021-10-10 00:45:44 +0530

    push

--- include.rb
+++
@@ -1,5 +0,0 @@
-require 'markdown_helper'
-
-# Option :pristine suppresses comment insertion.
-markdown_helper = MarkdownHelper.new(:pristine => true)
-markdown_helper.include('README.md', './Database/database.md')
